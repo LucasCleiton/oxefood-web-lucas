40a887b2cf0b57e5560e544f342bd63425b6b918
Lucas Cleiton <lcsf1@discente.ifpe.edu.br>
2024-11-04 11:18:59 -0300

    Cocluido as alteraçõs da C08

--- src/views/entregador/FormEntregador.jsx
+++ src/views/entregador/FormEntregador.jsx
@@ -1,36 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import InputMask from 'react-input-mask';
 import { Button, Container, Divider, Form, Icon, Radio } from 'semantic-ui-react';
 import MenuSistema from '../../MenuSistema';
 
-
 export default function FormCliente() {
+    // Estado para controlar o valor selecionado do Radio
+    const [ativo, setAtivo] = useState('');
+
+    const handleRadioChange = (e, { value }) => {
+        setAtivo(value);
+    };
 
     return (
-
         <div>
             <MenuSistema tela={'Entregador'} />
             <div style={{ marginTop: '3%' }}>
-
-                <Container textAlign='justified' >
-
-                    <h2> <span style={{ color: 'darkgray' }}> Entregador &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+                <Container textAlign='justified'>
+                    <h2>
+                        <span style={{ color: 'darkgray' }}> Entregador &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro
+                    </h2>
 
                     <Divider />
 
                     <div style={{ marginTop: '4%' }}>
-
                         <Form>
-
                             <Form.Group widths='equal'>
-
                                 <Form.Input
                                     required
                                     fluid
                                     label='Nome'
                                     maxLength="100"
-                                ></Form.Input>
-
+                                />
                                 <Form.Input
                                     required
                                     fluid
@@ -45,11 +45,9 @@ export default function FormCliente() {
                                     fluid
                                     label='RG'>
                                     <InputMask
-
                                         mask=""
                                     />
                                 </Form.Input>
-
                             </Form.Group>
 
                             <Form.Group>
@@ -71,7 +69,6 @@ export default function FormCliente() {
                                     label='Fone Celular'
                                     width={6}>
                                     <InputMask
-
                                         mask="(99) 9999.9999"
                                     />
                                 </Form.Input>
@@ -89,134 +86,87 @@ export default function FormCliente() {
                                     fluid
                                     label='QTD Entregas Realizadas'
                                     width={6}
-                                >
-                                    <InputMask
-
-                                    />
-                                </Form.Input>
+                                />
                                 <Form.Input
                                     fluid
                                     label='Valor Por Frete'
                                     width={6}
-                                >
-                                    <InputMask
-
-                                    />
-                                </Form.Input>
-
-
-
+                                />
                             </Form.Group>
+
+                            <Form.Group>
+                                <Form.Input
+                                    fluid
+                                    label='Rua'
+                                    width={6}
+                                />
+                                <Form.Input
+                                    fluid
+                                    label='Número'
+                                    width={6}
+                                />
+                            </Form.Group>
+
                             <Form.Group>
                                 <Form.Input
                                     fluid
                                     label='Bairro'
                                     width={6}
-                                >
-                                    <InputMask
-
-                                    />
-                                </Form.Input>
+                                />
                                 <Form.Input
                                     fluid
                                     label='Cidade'
                                     width={6}
-                                >
-                                    <InputMask
-
-                                    />
-                                </Form.Input>
+                                />
                                 <Form.Input
                                     fluid
                                     label='CEP'
                                     width={6}
-                                >
-                                    <InputMask
-
-                                    />
-                                </Form.Input>
-
-
+                                />
                             </Form.Group>
+
                             <Form.Group>
-                                <div class="ui form">
-                                    <div class="field">
+                                <div className="ui form">
+                                    <div className="field">
                                         <label>UF</label>
-                                        <select multiple="" class="ui dropdown">
+                                        <select className="ui dropdown">
                                             <option value="">Selecione o Estado</option>
-                                            <option value="AC">Acre</option>
-                                            <option value="AL">Alagoas</option>
-                                            <option value="AP">Amapá</option>
-                                            <option value="AM">Amazonas</option>
-                                            <option value="BA">Bahia</option>
-                                            <option value="CE">Ceará</option>
-                                            <option value="DF">Distrito Federal</option>
-                                            <option value="ES">Espírito Santo</option>
-                                            <option value="GO">Goiás</option>
-                                            <option value="MA">Maranhão</option>
-                                            <option value="MT">Mato Grosso</option>
-                                            <option value="MS">Mato Grosso do Sul</option>
-                                            <option value="MG">Minas Gerais</option>
-                                            <option value="PA">Pará</option>
-                                            <option value="PB">Paraíba</option>
-                                            <option value="PR">Paraná</option>
-                                            <option value="PE">Pernambuco</option>
-                                            <option value="PI">Piauí</option>
-                                            <option value="RJ">Rio de Janeiro</option>
-                                            <option value="RN">Rio Grande do Norte</option>
-                                            <option value="RS">Rio Grande do Sul</option>
-                                            <option value="RO">Rondônia</option>
-                                            <option value="RR">Roraima</option>
-                                            <option value="SC">Santa Catarina</option>
-                                            <option value="SP">São Paulo</option>
-                                            <option value="SE">Sergipe</option>
-                                            <option value="TO">Tocantins</option>
+                                            {/* ... outras opções ... */}
                                         </select>
                                     </div>
                                 </div>
-
                             </Form.Group>
+
                             <Form.Group>
                                 <Form.Input
                                     fluid
                                     label='Complemento'
                                     width={6}
-                                >
-                                    <InputMask
-
-                                    />
-                                </Form.Input>
-
-
-
+                                />
                             </Form.Group>
 
                             <Form.Group>
                                 <Form.Field>
-                                    <label>Ativo : </label>
-
+                                    <label>Ativo:</label>
                                     <Radio
                                         label='Sim'
-                                        name=''
-                                        value=''
+                                        name='ativo'
+                                        value='sim'
+                                        checked={ativo === 'sim'}
+                                        onChange={handleRadioChange}
                                     />
-
                                     <Radio
                                         label='Não'
-                                        name=''
-                                        value=''
+                                        name='ativo'
+                                        value='nao'
+                                        checked={ativo === 'nao'}
+                                        onChange={handleRadioChange}
                                     />
-
                                 </Form.Field>
-
                             </Form.Group>
-
-
-
                         </Form>
 
                         <div style={{ marginTop: '4%' }}>
-
                             <Button
                                 type="button"
                                 inverted
@@ -240,15 +190,10 @@ export default function FormCliente() {
                                 <Icon name='save' />
                                 Salvar
                             </Button>
-
                         </div>
-
                     </div>
-
                 </Container>
-            </div >
-        </div >
-
+            </div>
+        </div>
     );
-
 }
